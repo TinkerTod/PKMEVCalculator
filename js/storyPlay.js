752063b832dea7d53fc2dbfedcd64f4b2d00f1f8
TinkerTod <ashleighkateward@hotmail.com>
2023-07-09 06:04:52 +1000

    Commit updates to Javascipt

--- js/storyPlay.js
+++ js/storyPlay.js
@@ -2,7 +2,6 @@
 *	Check over the submission to make sure that 
 */
 function checking() {
-	var empty = 0;
 	$('input[type=text]').each(function(){
 		maxInput++;
 		if (this.value == "") {empty++;} 
@@ -14,74 +13,35 @@ function checking() {
 	}
 	else{alert(empty + ' empty input(s)')}
 }
-
+/**
+ *  Calculate the Vitamins and print
+ * */
 function appendData(){
-	for (i=1, max = maxInput; i < max; i++){
-		if(i < 10){
-			document.getElementById('val0'+ i).innerHTML = document.getElementById("in0" + i).value;	
-		}
-		else{
-			document.getElementById('val'+ i).innerHTML = document.getElementById("in" + i).value;
-		}
-	}
+    var Vit = 10;
+    var HP = document.getElementById('HP');
+    var Attack = document.getElementById('Attack');
+    var Defense = document.getElementById('Defense');
+    var SpAttack = document.getElementById('SpAttack');
+    var SpDef = document.getElementById('SpDef');
+    var Speed = document.getElementById('Speed');
+
+    var HPVit = math.round(HP/Vit);
+    var AttVit = math.round(Attack/Vit);
+    var DefVit = math.round(Defense/Vit);
+    var SpAttVit = math.round(SpAttack/Vit);
+    var SpDefVit = math.round(SpDef/Vit);
+    var SpeeVit = math.round(Speed/Vit);
+
+	document.getElementById('HPVit').innerHTML = HPVit.value;
+    document.getElementById('AttVit').innerHTML = AttVit.value;
+    document.getElementById('DefVit').innerHTML = DefVit.value;
+    document.getElementById('SpAttVit').innerHTML = SpAttVit.value;
+    document.getElementById('SPDefVit').innerHTML = SpDefVit.value;
+    document.getElementById('SpeeVit').innerHTML = SpeeVit.value;
 }
-
-
-function text2speech(){
-    if ('speechSynthesis' in window) {
-        var synth = speechSynthesis;
-        var flag = false;
-     
-        /* references to the buttons */
-        var playEle = document.querySelector('#play');
-        var pauseEle = document.querySelector('#pause');
-        var stopEle = document.querySelector('#stop');
-     
-        /* click event handlers for the buttons */
-        playEle.addEventListener('click', onClickPlay);
-        pauseEle.addEventListener('click', onClickPause);
-        stopEle.addEventListener('click', onClickStop);
-     
-        function onClickPlay() {
-            if(!flag){
-                flag = true;
-                utterance = new SpeechSynthesisUtterance(
-                      document.querySelector('#storycomplete').textContent);
-                utterance.voice = synth.getVoices()[0];
-                utterance.onend = function(){
-                    flag = false;
-                };
-                synth.speak(utterance);
-            }
-            if(synth.paused) { /* unpause/resume narration */
-                synth.resume();
-            }
-        }
-        function onClickPause() {
-            if(synth.speaking && !synth.paused){ /* pause narration */
-                synth.pause();
-            }
-        }
-        function onClickStop() {
-            if(synth.speaking){ /* stop narration */
-                /* for safari */
-                flag = false;
-                synth.cancel();
-            }
-        }
-    }
-}
-
-
-
-
-
-
-
 
 var startBtn = document.getElementById("startBtn");
 startBtn.onclick = function(){
 	checking();
 	appendData();
-	text2speech();
 }
