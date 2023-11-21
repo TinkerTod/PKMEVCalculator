94f8590a859d6c5dfbb93f25ee06df5381fbf1c4
TinkerTod <ashleighkateward@hotmail.com>
2023-11-21 16:58:14 +1100

    removal of guff

--- js/Calculation.js
+++ js/Calculation.js
@@ -1,24 +1,8 @@
-/*
-*	Check over the submission to make sure that 
-*/
-function checking() {
-	$('input[type=text]').each(function(){
-		maxInput++;
-		if (this.value == "") {empty++;} 
-	})
-	if (empty == 0){
-		alert("All inputs filled");
-		document.getElementById("inputArea").style.display="none";
-		document.getElementById("answer").style.display='block';
-	}
-	else{alert(empty + ' empty input(s)')}
-}
-/**
- *  Calculate the Vitamins and print
- * */
-function appendData(){
+var startBtn = document.getElementById("startBtn");
+startBtn.onclick = function(){
     var Vit = 10;
     var HP = parseInt(document.getElementById('HP').value);
+    window.alert("HP: "+ HP);
     var Attack = parseInt(document.getElementById('Attack').value);
     var Defense = parseInt(document.getElementById('Defense').value);
     var SpAttack = parseInt(document.getElementById('SpAttack').value);
@@ -32,16 +16,10 @@ function appendData(){
     var SpDefVit = math.round(SpDef/parseInt(Vit);
     var SpeeVit = math.round(Speed/parseInt(Vit);
 
-	document.getElementById('HPVit').innerHTML = HPVit;
+    document.getElementById('HPVit').innerHTML = HPVit;
     document.getElementById('AttVit').innerHTML = AttVit;
     document.getElementById('DefVit').innerHTML = DefVit;
     document.getElementById('SpAttVit').innerHTML = SpAttVit;
     document.getElementById('SPDefVit').innerHTML = SpDefVit;
     document.getElementById('SpeeVit').innerHTML = SpeeVit;
-}
-
-var startBtn = document.getElementById("startBtn");
-startBtn.onclick = function(){
-	checking();
-	appendData();
 }
